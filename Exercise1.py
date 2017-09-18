238fd0d9177cc0beec23d9c19c44ec195d729355
ChrisKarpinski <christopherkarpinski@cmail.carleton.ca>
2017-09-18 15:45:09 -0400

    Create Exercise1.py

---
+++ Exercise1.py
@@ -0,0 +1,10 @@
+# Exercise 1: 
+
+CURRENT_YEAR = 2017
+
+name = input("What is your name? ")
+
+yearOfBirth = input("What year were you born in? ")
+
+print(name + " must be between " + str(CURRENT_YEAR - int(yearOfBirth)) + " or " + str(CURRENT_YEAR - int(yearOfBirth) - 1) + " years old.")
+      
