0a85d2f5bb9c8c0bfacc9e9974b42943b334c535
ChrisKarpinski <christopherkarpinski@cmail.carleton.ca>
2017-09-18 15:47:05 -0400

    Create Exercise3.py

---
+++ Exercise3.py
@@ -0,0 +1,15 @@
+# Exercise 3: 
+
+days = input("How many days? ")
+
+weeks = int(days)/7
+
+months = int(days)/30
+
+years = int(days)/365
+
+print (days + " days is equivalent to " + str(weeks) + " weeks.")
+
+print (days + " days is equivalent to " + str(months) + " months.")
+
+print (days + " days is equivalent to " + str(years) + " years.")
